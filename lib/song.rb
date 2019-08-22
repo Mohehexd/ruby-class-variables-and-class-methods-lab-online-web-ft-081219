34ceb759938038c98dc225f272fdfc9da8816137
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-22 19:42:58 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  @@count = 0 
+  @@artist = []
+  @@genre = []
+  
+  def initialize (name, artist, genre)
+    @name = name 
+    @artist["Jay-Z", "Jay-Z", "Brittany Spears"] = artist
+    @genre = genre 
+    @@count +=1 
+  end 
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.genre 
+    @genre 
+  end 
+  
+  
+end 
+
