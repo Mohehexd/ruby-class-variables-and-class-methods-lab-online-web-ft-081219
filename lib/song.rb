11deaed0846f9b09e9f59e3f1f51196a9c948855
Guillermo Martinez <dumblemoe@gmail.com>
2019-08-22 20:25:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,13 @@
 class Song 
   attr_accessor :name, :artist, :genre 
   @@count = 0 
-  @@artist = []
-  @@genre = []
+  @@artists = []
+  @@genres = []
   
   def initialize (name, artist, genre)
     @name = name 
     @artist = artist
-    @@artist << artist
+    @@artists << artist
     @genre = genre 
     @@count +=1 
   end 
